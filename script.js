34bc071dc1eae21c747010589a1137b270090fe2
KaVamsiKrishna <kavamsi71@gmail.com>
2026-01-06 12:54:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const size = document.getElementById('sizeInfo');
+size.innerHTML = `<h1>Width: ${window.innerWidth} and Height: ${window.innerHeight}</h1>`
+window.addEventListener('resize', () =>{
+	size.innerHTML = `<h1>Width: ${window.innerWidth} and Height: ${window.innerHeight}</h1>`
+})
